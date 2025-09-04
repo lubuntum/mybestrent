010862948096ef7ed1179363b8f15364eb232009
lubuntum <lubuntum@gmail.com>
2025-09-04 11:19:39 +0700

    переименовано

--- src/components/apartament/ApartamentPage.jsx
+++
@@ -1,9 +0,0 @@
-import { useNavigate } from "react-router"
-
-export const ApartamentPage = () => {
-    const navigate = useNavigate()
-    return(<>
-        <p>Apartament</p>
-        <button onClick={() => navigate("/")}>Go Home</button>
-    </>)
-}
